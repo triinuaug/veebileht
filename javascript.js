e81ab66437491aedd862238df05deeab0364e0e6
Triinu Prants <triinu.prants@juvente.ee>
2019-11-29 20:12:54 +0200

    muudatus

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
   var images = new Array();
-  for (var i = 2; i < 13; i++) {
+  for (var i = 1; i < 13; i++) {
 	images.push("sherlock" + i + ".jpg");
   }
   var x = 0;
 
   function changeImage() {
 	document.getElementById('vahetuv').src = images[x];
-	if (x < 1) {
+	if (x < 11) {
 	  x += 1;
 	} else {
 	  x = 0;
@@ -15,5 +15,5 @@
   window.onload = function() {
 	setInterval(function () {
 	  changeImage();
-	}, 3500);
+	}, 2000);
   }
