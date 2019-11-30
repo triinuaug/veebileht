4c37d2208a3eeac1ae8650749d922e2f02c20be9
KristiinaKr <krausekristiina@gmail.com>
2019-11-30 19:06:46 +0200

    Mu arendajast vend editis veidi javascripti koodi, hope u dont mind :D

--- javascript.js
+++ javascript.js
@@ -1,19 +1,20 @@
-  var images = new Array();
-  for (var i = 1; i < 13; i++) {
-	images.push("sherlock" + i + ".jpg");
-  }
-  var x = 0;
+const images = [];
+for (let i = 1; i < 13; i++) {
+    images.push("sherlock" + i + ".jpg");
+}
+let x = 0;
 
-  function changeImage() {
-	document.getElementById('vahetuv').src = images[x];
-	if (x < 11) {
-	  x += 1;
-	} else {
-	  x = 0;
-	}
-  }
-  window.onload = function() {
-	setInterval(function () {
-	  changeImage();
-	}, 2000);
-  }
+function changeImage() {
+    document.getElementById('vahetuv').src = images[x];
+    if (x < 11) {
+        x += 1;
+    } else {
+        x = 0;
+    }
+}
+
+window.onload = function () {
+    setInterval(function () {
+        changeImage();
+    }, 2000);
+};
